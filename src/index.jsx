5d60ff13bf39a6ac19102acd7478eb7d2ff2b9a0
Wenny Xiong <wenny.xiong7@gmail.com>
2022-07-06 15:28:34 -0400

    initial setup

---
+++ src/index.jsx
@@ -0,0 +1,23 @@
+// Bring React in to build a component.
+import React from "react";
+// Bring reactDOM in to mount component to the dom.
+import reactDOM from "react-dom";
+
+// // Here is out base App component.
+// // Notice we are NOT using jsx here. This is because we have not set up babel yet.
+// const App = React.createElement("h1",null,"Hello World");
+
+// // Render our app to the dom mounted to the element with id of root inside our public/index.html file.
+// reactDOM.render(App, document.getElementById("root"));
+
+
+
+import { createRoot } from "react-dom/client";
+const root = createRoot(document.getElementById("root"));
+
+// Huzzah for jsx!
+const App = () => {
+  return <h1>Hello World</h1>
+}
+
+root.render(<App />);
