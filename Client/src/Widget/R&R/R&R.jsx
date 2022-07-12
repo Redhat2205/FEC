245d67ebbbf95b0de8d68766b90c14cb7709dbb8
Jordan Addleman <xibalbarising@gmail.com>
2022-07-12 00:57:34 -0400

    rendering from dummy data. conditional rendering stars. added jest unit test that tests if rendering from dummy data

--- Client/src/Widget/R&R/R&R.jsx
+++ Client/src/Widget/R&R/R&R.jsx
@@ -20,12 +20,47 @@ const Magic = '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';
 //   </div>
 // );
 
+const dummyData = [
+  {rating: 2,
+   summary: 'blah blah blaze blahblah',
+   date: '07-11-2022',
+   reviewer_name: 'shy guy',
+   body: 'dslajfdisfjidajfldsajflijlijflkdsjafldksjoigjasdoijgdisajfdigjisajgilsgjlidsajgil'
+  },
+  {rating: 3,
+   summary: 'pizza pizza pizza pizza',
+   date: '07-11-2022',
+   reviewer_name: 'pizza maniac',
+   body: 'adsfljkdsojgoiidsjgoidhfdhgljkfalfhdgfijiohweoirhgofshdihfighrowihgiohgioehgoi'
+  },
+  {rating: 5,
+   summary: '5 tacos plus rice and beans',
+   date: '07-11-2022',
+   reviewer_name: 'zeiram',
+   body: 'jaldfskjioewghaskhgjfdioshgiowrugroieuthdsaoghwrioiwohgiowahgoiefjo;gihrasagdhoi'
+  },
+  {rating: 1,
+   summary: 'behari beef kebab with rice, salad, chickpeas and bread',
+   date: '07-11-2022',
+   reviewer_name: 'Porsche Owner',
+   body: 'alksdjflahgoiewhgoiawhgiohgoijfaidoghriowhgoihwagiohfoghi'
+  },
+  {rating: 4,
+   summary: 'grilled chicken greek salad',
+   date: '07-11-2022',
+   reviewer_name: 'Giant Rabbit',
+   body: 'yuwihfgirosfhaeiodigosdjgjaghsflagsihiehegi'
+  },
+];
+
+const rando = Math.floor(Math.random() * dummyData.length);
+
 class R_R extends React.Component {
   constructor() {
     super();
     this.state = {
-      productReviews: [],
-      currentItem: {},
+      productReviews: dummyData,
+      currentItem: dummyData[rando],
     };
     // bind handlers in constructor
     this.fetchReviewsTest = this.fetchReviewsTest.bind(this);
@@ -36,30 +71,67 @@ class R_R extends React.Component {
   }
 
   fetchReviewsTest() {
-    axios({
-      method: 'get',
-      url: API_BASE + '/reviews',
-      headers: { Authorization: Magic },
-      params: { product_id: '37320' },
-    })
-      .then((res) => {
-        this.setState({ productReviews: res.data.results });
-        this.setState({ currentItem: res.data.results[0] });
-      })
-      .catch((err) => {
-        console.log('🟥there was an error fetching product info!', err);
-      });
+    // axios({
+    //   method: 'get',
+    //   url: API_BASE + '/reviews',
+    //   headers: { Authorization: Magic },
+    //   params: { product_id: '37320' },
+    // })
+    //   .then((res) => {
+    //     this.setState({ productReviews: res.data.results });
+    //     this.setState({ currentItem: res.data.results[0] });
+    //   })
+    //   .catch((err) => {
+    //     console.log('🟥there was an error fetching product info!', err);
+    //   });
+
+    // please give me the API authorization back!!!
+    // *******************************************
+    // const dummyData = [
+    //   {rating: 2,
+    //    summary: 'blah blah blaze blahblah',
+    //    date: '07-11-2022',
+    //    reviewer_name: 'shy guy',
+    //    body: 'dslajfdisfjidajfldsajflijlijflkdsjafldksjoigjasdoijgdisajfdigjisajgilsgjlidsajgil'
+    //   },
+    //   {rating: 3,
+    //    summary: 'pizza pizza pizza pizza',
+    //    date: '07-11-2022',
+    //    reviewer_name: 'pizza maniac',
+    //    body: 'adsfljkdsojgoiidsjgoidhfdhgljkfalfhdgfijiohweoirhgofshdihfighrowihgiohgioehgoi'
+    //   },
+    //   {rating: 5,
+    //    summary: '5 tacos plus rice and beans',
+    //    date: '07-11-2022',
+    //    reviewer_name: 'zeiram',
+    //    body: 'jaldfskjioewghaskhgjfdioshgiowrugroieuthdsaoghwrioiwohgiowahgoiefjo;gihrasagdhoi'
+    //   },
+    //   {rating: 1,
+    //    summary: 'behari beef kebab with rice, salad, chickpeas and bread',
+    //    date: '07-11-2022',
+    //    reviewer_name: 'Porsche Owner',
+    //    body: 'alksdjflahgoiewhgoiawhgiohgoijfaidoghriowhgoihwagiohfoghi'
+    //   },
+    //   {rating: 4,
+    //    summary: 'grilled chicken greek salad',
+    //    date: '07-11-2022',
+    //    reviewer_name: 'Giant Rabbit',
+    //    body: 'yuwihfgirosfhaeiodigosdjgjaghsflagsihiehegi'
+    //   },
+    // ];
+    // this.setState({productReviews: dummyData})
+    // this.setState({currentItem: this.state.productReviews[0]});
   }
 
   render() {
     return (
       <div className="rnr-container">
-        <h1 style={{textAlign: 'center'}}>Ratings and Reviews</h1>
-        <div className="rating-chart-container" style={{ border: 'solid 2px', float: 'left', width: '49%' }}>
+        <h1 data-testid="rnr" style={{textAlign: 'center', fontFamily: 'tahoma'}}>Ratings and Reviews</h1>
+        <div className="rating-chart-container" style={{ border: 'solid 1px', borderRadius: '5px', boxShadow: '5px 10px #888888', float: 'left', width: '33%' }}>
           <RatingChart currentItem={this.state.currentItem} />
         </div>
-        <div className="review-list-container" style={{ border: 'solid 2px', float: 'right', width: '49%' }}>
-          <h3 style={{textAlign: 'center'}}>Review List</h3>
+        <div className="review-list-container" style={{ border: 'solid 1px', borderRadius: '5px', boxShadow: '5px 10px #888888', float: 'right', width: '66%' }}>
+          <h1 style={{textAlign: 'center', fontFamily: 'Tahoma'}}>Review List</h1>
           <ReviewList reviews={this.state.productReviews} />
         </div>
       </div>
