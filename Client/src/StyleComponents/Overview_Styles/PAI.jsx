df1ed597e9ab7843b00ea28e792713a967e963a1
Wenny Xiong <wenny.xiong7@gmail.com>
2022-07-12 22:25:42 -0400

    mostly finished product additional info section

--- Client/src/StyleComponents/Overview_Styles/PAI.jsx
+++ Client/src/StyleComponents/Overview_Styles/PAI.jsx
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 const PAI = {
   Slogan: styled.div`
-    display: block;
+    display: flex;
+    float: left;
     margin: 30px 0px 20px 150px;
     width: 40%;
     height: 20%;
@@ -10,11 +11,21 @@ const PAI = {
     font-size: 20px;
   `,
   Description: styled.div`
-    display: block;
+    display: flex;
+    float: left;
     margin: 10px 0px 20px 150px;
     width: 40%;
+    height: 50%;
+    font-size: 15px;
+  `,
+  FeatureList: styled.ul`
+    border-left: 2px solid #B2B4B8;
+    float: right;
+    margin: 30px 150px 20px 20px;
+    width: 25%;
     height: 70%;
     font-size: 15px;
+    line-height: 2;
   `,
 };
 
--- Client/src/Widget/Overview/Components/ProductAdditionalInfo.jsx
+++ Client/src/Widget/Overview/Components/ProductAdditionalInfo.jsx
@@ -2,15 +2,36 @@ import React from "react";
 import SectionDiv from "../../../StyleComponents/Overview_Styles/SectionDiv.jsx";
 import PAI from "../../../StyleComponents/Overview_Styles/PAI.jsx";
 
-const ProductAdditionalInfo = ({ product, currStyle }) => (
-  <SectionDiv.ProductAdditionalInfo>
-    <PAI.Slogan>
-      {product.slogan}
-    </PAI.Slogan>
-    <PAI.Description>
-      {product.description}
-    </PAI.Description>
-  </SectionDiv.ProductAdditionalInfo>
-);
+const ProductAdditionalInfo = ({ product, currStyle }) => {
+  if (product.name !== undefined) {
+    console.log('product features: ', product.features);
+    return (
+      <SectionDiv.ProductAdditionalInfo>
+        <PAI.Slogan>
+          {product.slogan}
+        </PAI.Slogan>
+        <PAI.FeatureList>
+          {product.features.map((eachFeature) => (
+            <li>
+              &nbsp;
+              {eachFeature.feature}
+              :&nbsp;
+              {eachFeature.value}
+            </li>
+          ))}
+        </PAI.FeatureList>
+        <PAI.Description>
+          {product.description}
+        </PAI.Description>
+
+      </SectionDiv.ProductAdditionalInfo>
+    );
+  }
+  return (
+    <SectionDiv.ProductAdditionalInfo>
+      Product Slogan and Description Section
+    </SectionDiv.ProductAdditionalInfo>
+  );
+};
 
 export default ProductAdditionalInfo;
