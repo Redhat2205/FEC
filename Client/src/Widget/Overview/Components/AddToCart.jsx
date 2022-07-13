dac5bca33cbf2be174d4b035bcce2b5bf079e037
Wenny Xiong <wenny.xiong7@gmail.com>
2022-07-13 19:21:13 -0400

    add to cart: when style is selected, set default quantity to 1

--- Client/src/Widget/Overview/Components/AddToCart.jsx
+++ Client/src/Widget/Overview/Components/AddToCart.jsx
@@ -3,7 +3,7 @@ import SectionDiv from "../../../StyleComponents/Overview_Styles/SectionDiv.jsx"
 import ATC from "../../../StyleComponents/Overview_Styles/ATC.jsx";
 
 const AddToCart = ({ currStyle }) => {
-  const defaultStock = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  const defaultStock = ['-', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
   const [size, setSize] = useState(null);
   const [stock, setStock] = useState(defaultStock);
@@ -13,24 +13,30 @@ const AddToCart = ({ currStyle }) => {
     const skus = Object.entries(currStyle.skus);
 
     const selectSizeAndUpdateStock = (e) => {
-      setSize(e.target.value);
+      console.log('target value size: ', e.target.value);
+      if (e.target.value === 'Select Size') {
+        setStock(defaultStock);
+        setQuantity(defaultStock[0]);
+      } else {
+        setSize(e.target.value);
 
-      skus.forEach((sku) => {
-        if (sku[1].size === e.target.value) {
-          let newStockNum = sku[1].quantity;
-          const newStockArr = [];
+        skus.forEach((sku) => {
+          if (sku[1].size === e.target.value) {
+            let newStockNum = sku[1].quantity;
+            const newStockArr = [];
 
-          if (newStockNum >= 15) {
-            setStock(defaultStock);
-          } else {
-            while (newStockNum > 0) {
-              newStockArr.unshift(newStockNum);
-              newStockNum -= 1;
+            if (newStockNum >= 15) {
+              setStock(defaultStock.slice(1));
+            } else {
+              while (newStockNum > 0) {
+                newStockArr.unshift(newStockNum);
+                newStockNum -= 1;
+              }
+              setStock(newStockArr);
             }
-            setStock(newStockArr);
           }
-        }
-      });
+        });
+      }
     };
 
     return (
@@ -47,7 +53,7 @@ const AddToCart = ({ currStyle }) => {
         </ATC.SelectSize>
 
         <ATC.SelectQuantity>
-          <option> - </option>
+          {/* <option> - </option> */}
           { size === null
             ? stock.map((quantityChoice, index) => (
               <option
