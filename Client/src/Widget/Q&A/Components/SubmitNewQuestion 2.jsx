0610ff9dfe8cb35c4b671d9dab658e8bc64fef3e
Wenny Xiong <wenny.xiong7@gmail.com>
2022-07-14 11:43:56 -0400

    update files

--- Client/src/Widget/Q&A/Components/SubmitNewQuestion 2.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const SubmitNewQuestion = ({ qA }) => (
-  <div>
-    <button type="button">Submit New Question</button>
-  </div>
-);
-
-export default SubmitNewQuestion;
--- Client/src/Widget/R&R/MoreReviews 2.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-
-const MoreReviews = () => {
-  return (
-    <div style={{width: '180px', height: '25px', textAlign: 'center', float: 'left', border: 'solid 1px', padding: '25px 0 25px', margin: '4px'}}>MORE REVIEWS</div>
-  );
-};
-
-export default MoreReviews;
