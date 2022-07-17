3594bf580cc4eec1deb5244a1819cda4925718d3
Jordan Addleman <xibalbarising@gmail.com>
2022-07-16 23:47:12 -0400

    linter

--- Client/src/Widget/R&R/ReviewTile.jsx
+++ Client/src/Widget/R&R/ReviewTile.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-const moment = require('moment');
+
 import ReviewImages from './ReviewImages.jsx';
+
+const moment = require('moment');
 
 // styles
 const textRight = {
@@ -27,7 +29,7 @@ const ReviewTile = ({ eachReview }) => {
       if (eachReview.response) {
         return (
           <div className="review-tile-container" style={tileBottomBorder}>
-            <h5 style={textRight}>{eachReview.reviewer_name + ', ' + moment(eachReview.date).format('MMMM Do YYYY')}</h5>
+            <h5 style={textRight}>{`${eachReview.reviewer_name}, ${moment(eachReview.date).format('MMMM Do YYYY')}`}</h5>
             <h3>Review Title</h3>
             <h5>{eachReview.summary}</h5>
             <p>{eachReview.body}</p>
@@ -36,96 +38,97 @@ const ReviewTile = ({ eachReview }) => {
             <div style={greyBG}>{eachReview.response}</div>
           </div>
         );
-      } else { // no response given!
-        return (
-          <div className="review-tile-container" style={tileBottomBorder}>
-            <h5 style={textRight}>{eachReview.reviewer_name + ', ' + moment(eachReview.date).format('MMMM Do YYYY')}</h5>
-            <h3>Review Title</h3>
-            <h5>{eachReview.summary}</h5>
-            <p>{eachReview.body}</p>
-            <ReviewImages images={eachReview.photos} />
-            <p>✔︎ I recommend this product</p>
-          </div>
-        );
-      }
-    } else { // not recommended!
-      // response given!!!
-      if (eachReview.response) {
-        return (
-          <div className="review-tile-container" style={tileBottomBorder}>
-            <h5 style={textRight}>{eachReview.reviewer_name + ', ' + moment(eachReview.date).format('MMMM Do YYYY')}</h5>
-            <h3>Review Title</h3>
-            <h5>{eachReview.summary}</h5>
-            <p>{eachReview.body}</p>
-            <ReviewImages images={eachReview.photos} />
-            <div style={greyBG}>{eachReview.response}</div>
-          </div>
-        );
-      } else {
-        // no response given!!!
-        return (
-          <div className="review-tile-container" style={tileBottomBorder}>
-            <h5 style={textRight}>{eachReview.reviewer_name + ', ' + moment(eachReview.date).format('MMMM Do YYYY')}</h5>
-            <h3>Review Title</h3>
-            <h5>{eachReview.summary}</h5>
-            <p>{eachReview.body}</p>
-            <ReviewImages images={eachReview.photos} />
-          </div>
-        );
-      }
+      } // no response given!
+      return (
+        <div className="review-tile-container" style={tileBottomBorder}>
+          <h5 style={textRight}>{`${eachReview.reviewer_name}, ${moment(eachReview.date).format('MMMM Do YYYY')}`}</h5>
+          <h3>Review Title</h3>
+          <h5>{eachReview.summary}</h5>
+          <p>{eachReview.body}</p>
+          <ReviewImages images={eachReview.photos} />
+          <p>✔︎ I recommend this product</p>
+        </div>
+      );
+    } // not recommended!
+    // response given!!!
+    if (eachReview.response) {
+      return (
+        <div className="review-tile-container" style={tileBottomBorder}>
+          <h5 style={textRight}>{eachReview.reviewer_name + ', ' + moment(eachReview.date).format('MMMM Do YYYY')}</h5>
+          <h3>Review Title</h3>
+          <h5>{eachReview.summary}</h5>
+          <p>{eachReview.body}</p>
+          <ReviewImages images={eachReview.photos} />
+          <div style={greyBG}>{eachReview.response}</div>
+        </div>
+      );
     }
-  } else {
-    // if no image <========================================================>
-    // recommended!
-    if (eachReview.recommend === true) {
-      // response given!
-      if (eachReview.response) {
-        return (
-          <div className="review-tile-container" style={tileBottomBorder}>
-            <h5 style={textRight}>{eachReview.reviewer_name + ', ' + moment(eachReview.date).format('MMMM Do YYYY')}</h5>
-            <h3>Review Title</h3>
-            <h5>{eachReview.summary}</h5>
-            <p>{eachReview.body}</p>
-            <p>✔︎ I recommend this product</p>
-            <div style={greyBG}>{eachReview.response}</div>
-          </div>
-        );
-      } else { // no response given!
-        return (
-          <div className="review-tile-container" style={tileBottomBorder}>
-            <h5 style={textRight}>{eachReview.reviewer_name + ', ' + moment(eachReview.date).format('MMMM Do YYYY')}</h5>
-            <h3>Review Title</h3>
-            <h5>{eachReview.summary}</h5>
-            <p>{eachReview.body}</p>
-            <p>✔︎ I recommend this product</p>
-          </div>
-        );
-      }
-    } else { // not recommended!
-      // response given!!!
-      if (eachReview.response) {
-        return (
-          <div className="review-tile-container" style={tileBottomBorder}>
-            <h5 style={textRight}>{eachReview.reviewer_name + ', ' + moment(eachReview.date).format('MMMM Do YYYY')}</h5>
-            <h3>Review Title</h3>
-            <h5>{eachReview.summary}</h5>
-            <p>{eachReview.body}</p>
-            <div style={greyBG}>{eachReview.response}</div>
-          </div>
-        );
-      } else {
-        // no response given!!!
-        return (
-          <div className="review-tile-container" style={tileBottomBorder}>
-            <h5 style={textRight}>{eachReview.reviewer_name + ', ' + moment(eachReview.date).format('MMMM Do YYYY')}</h5>
-            <h3>Review Title</h3>
-            <h5>{eachReview.summary}</h5>
-            <p>{eachReview.body}</p>
-          </div>
-        );
-      }
-    }
+    // no response given!!!
+    return (
+      <div className="review-tile-container" style={tileBottomBorder}>
+        <h5 style={textRight}>{`${eachReview.reviewer_name}, ${moment(eachReview.date).format('MMMM Do YYYY')}`}</h5>
+        <h3>Review Title</h3>
+        <h5>{eachReview.summary}</h5>
+        <p>{eachReview.body}</p>
+        <ReviewImages images={eachReview.photos} />
+      </div>
+    );
   }
+  // if no image <========================================================>
+  // recommended!
+  if (eachReview.recommend === true) {
+    // response given!
+    if (eachReview.response) {
+      return (
+        <div className="review-tile-container" style={tileBottomBorder}>
+          <h5 style={textRight}>{`${eachReview.reviewer_name}, ${moment(eachReview.date).format('MMMM Do YYYY')}`}</h5>
+          <h3>Review Title</h3>
+          <h5>{eachReview.summary}</h5>
+          <p>{eachReview.body}</p>
+          <p>✔︎ I recommend this product</p>
+          <div style={greyBG}>{eachReview.response}</div>
+        </div>
+      );
+    } // no response given!
+    return (
+      <div className="review-tile-container" style={tileBottomBorder}>
+        <h5 style={
+              textRight
+              }
+        >
+          {`${eachReview.reviewer_name  }, ${  moment(eachReview.date).format('MMMM Do YYYY')}`}
+        </h5>
+        <h3>Review Title</h3>
+        <h5>{eachReview.summary}</h5>
+        <p>{eachReview.body}</p>
+        <p>✔︎ I recommend this product</p>
+      </div>
+    );
+  } // not recommended!
+  // response given!!!
+  if (eachReview.response) {
+    return (
+          <div className="review-tile-container" style={tileBottomBorder}>
+            <h5 style={textRight}>{eachReview.reviewer_name + ', ' + moment(eachReview.date).format('MMMM Do YYYY')}</h5>
+            <h3>Review Title</h3>
+            <h5>{eachReview.summary}</h5>
+            <p>{eachReview.body}</p>
+            <div style={greyBG}>{eachReview.response}</div>
+          </div>
+        );
+      } else {
+        // no response given!!!
+        return (
+          <div className="review-tile-container" style={tileBottomBorder}>
+            <h5 style={textRight}>{eachReview.reviewer_name + ', ' + moment(eachReview.date).format('MMMM Do YYYY')}</h5>
+            <h3>Review Title</h3>
+            <h5>{eachReview.summary}</h5>
+            <p>{eachReview.body}</p>
+          </div>
+        );
+      }
+
+
 };
 
 export default ReviewTile;
