3d779bfd71b567d576388fb74ae26161d38e14aa
Jordan Addleman <xibalbarising@gmail.com>
2022-07-17 03:04:52 -0400

    dynamically render the percentage of reviews marked as helpful

--- Client/src/Widget/R&R/RatingChart.jsx
+++ Client/src/Widget/R&R/RatingChart.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import BasicFn from './BasicFn.jsx';
 import BarChartDemo from './BarChartDemo.jsx';
+import Recommended from './Recommended.jsx';
 
 // eslint-disable-next-line consistent-return
 const RatingChart = ({ currentItem, averageRating, productReviews }) => (
   <div>
     <h5 style={{ display: 'inline-block' }}>{averageRating}</h5>
     <BasicFn averageRating={averageRating} style={{ display: 'inline-block' }} />
+    <Recommended productReviews={productReviews} />
     <div>
       <BarChartDemo productReviews={productReviews} />
       <p>
