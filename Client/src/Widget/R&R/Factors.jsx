5cf6b866e0cf86865398c7251adad4709c3a7b5c
Jordan Addleman <xibalbarising@gmail.com>
2022-07-19 17:36:42 -0400

    cleaned up console logs and all but 2 linter spikes. rating chart renders all qualities & point values for current item

--- Client/src/Widget/R&R/Factors.jsx
+++ Client/src/Widget/R&R/Factors.jsx
@@ -1,33 +1,21 @@
 import React from 'react';
 
 // eslint-disable-next-line consistent-return
-const Factors = ({ productReviews, metaData }) => {
-  if (productReviews && metaData) {
-    // const size = productReviews.map((review) => );
-    const sizeCount = 0;
-    // const wide = productReviews.map();
-    const wideCount = 0;
-    // const comfort = productReviews.map();
-    const comfortCount = 0;
-    // const quality = productReviews.map();
-    const qualityCount = 0;
-    // const length = productReviews.map();
-    const lengthCount = 0;
-    // const fit = productReviews.map();
-    const fitCount = 0;
-    // MAKE A CALL TO META TO REFACTOR SOME OF THE RATING CHART STUFF
-    // NOT ALL PRODUCTS HAVE ALL QUALITIES IT SEEMS
-    // productReviews.forEach((review) => {
-    //   sizeCount += review.
-    // })
+const Factors = ({ productReviews, characteristics }) => {
+  if (productReviews && characteristics) {
+    const chara = Object.entries(characteristics);
 
+    const mappers = [];
+
+    chara.forEach((key, value) => {
+      mappers.push([key[0], Number(key[1].value).toFixed(2)]);
+    });
+
+    console.log(mappers);
     return (
       <ul>
-        {/* {Object.keys(metaData.characteristics).map((characteristic, index, obj) => (
-          <li>{characteristic}<span>{`${obj[index]}`}</span></li>
-        ))} */}
-        {Object.entries(metaData.characteristics).map((characteristic, index, obj) => (
-          <li key={index}>{`${characteristic} `}</li>
+        {mappers.map((arr, index) => (
+          <li key={index}>{`${arr[0]} : ${arr[1]}`}</li>
         ))}
       </ul>
     );
--- Client/src/Widget/R&R/RatingChart.jsx
+++ Client/src/Widget/R&R/RatingChart.jsx
@@ -7,18 +7,22 @@ import Factors from './Factors.jsx';
 // eslint-disable-next-line consistent-return
 const RatingChart = ({
   currentItem, averageRating, productReviews, metaData,
-}) => (
-  <div>
-    <h5 style={{ display: 'inline-block' }}>{averageRating}</h5>
-    <BasicFn averageRating={averageRating} style={{ display: 'inline-block' }} />
-    <Recommended productReviews={productReviews} />
-    <div>
-      <BarChartDemo productReviews={productReviews} />
-    </div>
-    <div>
-      <Factors productReviews={productReviews} metaData={metaData} />
-    </div>
-  </div>
-);
+}) => {
+  if (metaData && productReviews && averageRating) {
+    return (
+      <div>
+        <h5 style={{ display: 'inline-block' }}>{averageRating}</h5>
+        <BasicFn averageRating={averageRating} style={{ display: 'inline-block' }} />
+        <Recommended productReviews={productReviews} />
+        <div>
+          <BarChartDemo productReviews={productReviews} />
+        </div>
+        <div>
+          <Factors productReviews={productReviews} characteristics={metaData.characteristics} />
+        </div>
+      </div>
+    );
+  }
+};
 
 export default RatingChart;
